f716aa7915edff63b35b013b79f1a7a2cae614bb
Pablo Santos Bayona <psantosb@Ordenador-portatil-de-Pablo.local>
2026-08-23 22:48:06 +0200

    feat: add validated supabase client

---
+++ src/lib/supabase/client.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+import { env } from "../env";
+
+export const supabase = createClient(
+  env.PUBLIC_SUPABASE_URL,
+  env.PUBLIC_SUPABASE_ANON_KEY,
+);
